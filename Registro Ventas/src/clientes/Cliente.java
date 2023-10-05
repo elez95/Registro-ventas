2dcc8c25dcfe49fcee8394ee81704fd8353bf8f8
Elez95 <eliasg.videla@gmail.com>
2023-10-05 17:05:34 -0300

    unifico el crud de cliente en una clase, elimino las demas

--- Registro Ventas/src/clientes/Cliente.java
+++ Registro Ventas/src/clientes/Cliente.java
@@ -1,60 +1,126 @@
 package clientes;
 
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import ConexionCliente.DeleteCliente;
-import ConexionCliente.InsertCliente;
-import ConexionCliente.ReadCliente;
+import java.sql.Statement;
+import conexion.Conexion;
 
 public class Cliente {
 
-	
-	private int id_cliente;
-	private String nombre_cliente;
-	
-	
-	
-	
-	
-	public void agregar_Cliente(String nombre_cliente) {
-		
-		InsertCliente.insert_cliente(nombre_cliente);
-	
-		
-	}
-	
-	public void editar_Cliente() {}
-	
-	public void eliminar_Cliente(String nombre_cliente) {
-		
-		DeleteCliente.delete_cliente(nombre_cliente);
-		
+
+	public static void create_cliente(String nombre) {
+		Conexion conexion = new Conexion();
+		Connection cn = null;
+		cn = conexion.conectar();
+
+		String insertQuery = "insert into clientes (nombre) values(?);";
+
+		try (Connection connection = cn;
+				PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
+
+			// Setear los valores de las columnas
+			preparedStatement.setString(1, nombre);
+			//preparedStatement.setInt(2, edad);
+			//preparedStatement.setInt(3, id);
+
+			// Ejecutar la inserción
+			int rowsInserted = preparedStatement.executeUpdate();
+			if (rowsInserted > 0) {
+				System.out.println("Inserción exitosa, " + rowsInserted + " columna/s afectadas");
+			}
+
+		} catch (SQLException e) {
+			System.out.println("hola");
+			e.printStackTrace();
+
+		}
+
 	}
 
-	public void leer_clientes() {
-		ReadCliente.leerTabla();
-	}
-	
-	
-	
-	public String getNombre_cliente() {
-		return nombre_cliente;
+	public static void read_tabla() {
+		
+		Conexion c = new Conexion();
+		try (Connection conexion = c.conectar();
+				Statement statement = conexion.createStatement()) {
+
+			// Ejecutar una consulta SQL para seleccionar todos los registros de la tabla
+			String consultaSQL = "SELECT * FROM clientes";
+			ResultSet resultado = statement.executeQuery(consultaSQL);
+
+			// Recorrer el resultado y mostrar los datos
+			while (resultado.next()) {
+				// Reemplaza "columna1" y "columna2" con los nombres de las columnas de tu tabla
+				String nombreCliente = resultado.getString("nombre");
+				int idCliente = resultado.getInt("id");
+				System.out.println("Id: " + idCliente + " Nombre: " + nombreCliente );
+			}
+
+		} catch (SQLException e) {
+			System.out.println("Error al leer la tabla");
+			e.printStackTrace();
+		}
 	}
 
-	public int getId_cliente() {
-		return id_cliente;
+	
+	// Método para actualizar un registro por su ID y nuevo nombre
+    public static void update_cliente(int id, String nuevoNombre) {
+    	
+        String sentenciaSQL = "UPDATE clientes SET nombre = ? WHERE id = ?";
+        Conexion c = new Conexion();
+        try (Connection conexion = c.conectar();
+             PreparedStatement preparedStatement = conexion.prepareStatement(sentenciaSQL)) {
+
+            // Establece los parámetros en la sentencia SQL
+            preparedStatement.setString(1, nuevoNombre);
+            preparedStatement.setInt(2, id);
+
+            // Ejecuta la sentencia SQL de actualización
+            int filasAfectadas = preparedStatement.executeUpdate();
+
+            // Imprime el número de filas afectadas por la actualización
+            System.out.println("Filas afectadas: " + filasAfectadas);
+        } catch (SQLException e) {
+            System.out.println("Error al ejecutar la sentencia SQL de actualización");
+            e.printStackTrace();
+        }
+    }
+	
+
+	public static void delete_cliente(String nombre_cliente) {
+
+		Conexion conexion = new Conexion();
+		Connection cn = null;
+		cn = conexion.conectar();
+
+		String deleteQuery = "DELETE from clientes WHERE nombre = '" + nombre_cliente + "';";
+
+		try(Connection connection = cn;
+				PreparedStatement preparedstatement = connection.prepareStatement(deleteQuery)){
+
+			int rowsDeleted = preparedstatement.executeUpdate(deleteQuery);
+
+			if(rowsDeleted > 0) {
+				System.out.println("Delete exitoso, " + rowsDeleted + " columna/s afectadas");
+			}
+
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
-	
-	
 
 	public static void main(String args []) throws SQLException {
-		
-		Cliente c = new Cliente();
-		c.agregar_Cliente("Milei");
-		c.leer_clientes();
-		//c.eliminar_Cliente("Lucas");
-		
+
+
+		//Cliente.create_cliente("Milei");
+		//Cliente.delete_cliente("Milei");
+		Cliente.update_cliente(11, "Palandri");
+		Cliente.read_tabla();
+
+
 	}
-	
+
 }
